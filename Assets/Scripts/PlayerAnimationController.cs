e567f09b6c9e1f6ddcc4670e8fa913db9e39d3a8
JIChoiz <h22334496@naver.com>
2018-08-06 16:51:44 +0900

    ver_0.1
    기본적인 채팅기능 및 캐릭터 이모션,사운드

---
+++ Assets/Scripts/PlayerAnimationController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+    [Header("SFX: Handclap")]
+    [SerializeField] AudioClip[] acHandclap;
+    [SerializeField] AudioClip[] acFootstep;
+
+    Animator animator;
+    AudioSource audioSource;
+
+	void Start ()
+    {
+        animator = GetComponent<Animator>();
+
+        audioSource = gameObject.AddComponent<AudioSource>();
+    }
+	
+	void Update ()
+    {
+		
+	}
+
+    void EmotionEnd()
+    {
+        animator.SetBool("Doing Emotion", false);
+    }
+
+    void Handclap()
+    {
+        int nRand = Random.Range(0, acHandclap.Length - 1);
+
+        audioSource.PlayOneShot(acHandclap[nRand]);
+    }
+
+    void Footstep()
+    {
+        int nRand = Random.Range(0, acFootstep.Length - 1);
+
+        audioSource.PlayOneShot(acFootstep[nRand]);
+    }
+}
