0c6ef6079c98c749cb5a185c560c84148b6417e7
Simple <simple3068@naver.com>
2018-08-07 18:23:36 +0900

    Attach SFX onto Animations
    * 애니메이션에 감정을 더욱 효과적으로 표현할 수 있는 SFX를 추가하였습니다.
      - 일부 애니메이션은 SFX보다 플레이타임이 길어 조금 어색할 수 있습니다.
      - 따라서 이를 수정해야 할 것입니다.
    * SFX를 추가해도 애니메이션이 조금 밋밋한 느낌이 들고 있습니다.
      - 이를 해결하기 위해 이펙트를 조금 추가해 볼 계획입니다.
    * 플레이어의 이동 문제
      - master 브랜치에서부터 발생한 문제인데, 플레이어가 자꾸 제자리걸음을 합니다.
      - 이는 프로젝트의 스케일이 변경되었지만 플레이어의 이동 속도는 변경하지 않아서 매우 느리게 이동하는 것이 거시적으로는 정지한 것처럼 보이기 때문입니다.
      - 아직 팀원간 스케일 조정에 관한 합의를 하지 않았기 때문에 이 문제는 그대로 남겨둡니다.

--- Assets/Scripts/PlayerAnimationController.cs
+++ Assets/Scripts/PlayerAnimationController.cs
@@ -4,9 +4,30 @@ using UnityEngine;
 
 public class PlayerAnimationController : MonoBehaviour
 {
+    [Header("SFX: Basic Emotions")]
+    [SerializeField] AudioClip acAroused;
+    [SerializeField] AudioClip acExcited;
+    [SerializeField] AudioClip acPleasured;
+    [SerializeField] AudioClip acContent;
+    [SerializeField] AudioClip acSleepy;
+    [SerializeField] AudioClip acDepressed;
+    [SerializeField] AudioClip acMiserious;
+    [SerializeField] AudioClip acDistressed;
+    [SerializeField] AudioClip acHello;
+    [SerializeField] AudioClip acThanks;
+    [SerializeField] AudioClip acSorry;
+    [SerializeField] AudioClip acCongrats;
+    [SerializeField] AudioClip acEmbarrassed;
+    [SerializeField] AudioClip acNo;
+    [SerializeField] AudioClip acViolent;
+    [SerializeField] AudioClip acPoint;
     [Header("SFX: Handclap")]
     [SerializeField] AudioClip[] acHandclap;
+    [Header("SFX: Footstep")]
     [SerializeField] AudioClip[] acFootstep;
+    [Header("SFX: Punch Whoosh")]
+    [SerializeField] AudioClip[] acWhoosh;
+
 
     Animator animator;
     AudioSource audioSource;
@@ -40,5 +61,35 @@ public class PlayerAnimationController : MonoBehaviour
         int nRand = Random.Range(0, acFootstep.Length - 1);
 
         audioSource.PlayOneShot(acFootstep[nRand]);
+    }
+
+    void PunchWhoosh()
+    {
+        int nRand = Random.Range(0, acWhoosh.Length - 1);
+
+        audioSource.PlayOneShot(acWhoosh[nRand]);
+    }
+
+    void EmotionSound(int nEmotion)
+    {
+        switch(nEmotion)
+        {
+            case 1: audioSource.PlayOneShot(acAroused); break;
+            case 2: audioSource.PlayOneShot(acExcited); break;
+            case 3: audioSource.PlayOneShot(acPleasured); break;
+            case 4: audioSource.PlayOneShot(acContent); break;
+            case 5: audioSource.PlayOneShot(acSleepy); break;
+            case 6: audioSource.PlayOneShot(acDepressed); break;
+            case 7: audioSource.PlayOneShot(acMiserious); break;
+            case 8: audioSource.PlayOneShot(acDistressed); break;
+            case 9: audioSource.PlayOneShot(acHello); break;
+            case 10: audioSource.PlayOneShot(acThanks); break;
+            case 11: audioSource.PlayOneShot(acSorry); break;
+            case 12: audioSource.PlayOneShot(acCongrats); break;
+            case 13: audioSource.PlayOneShot(acEmbarrassed); break;
+            case 14: audioSource.PlayOneShot(acNo); break;
+            case 15: audioSource.PlayOneShot(acViolent); break;
+            case 16: audioSource.PlayOneShot(acPoint); break;
+        }
     }
 }
